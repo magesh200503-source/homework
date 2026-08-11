5580a9dfab42966038946dd2a4ddd2363fd963cc
magesh <magesh200503@gmail.com>
2026-08-11 05:00:35 +0300

    Add files via upload

---
+++ main-31/main-31.js
@@ -0,0 +1,44 @@
+// Задача 1.
+// Напишите функцию calculateFinalPrice, которая принимает базовую цену товара, процент скидки и налоговую ставку. Функция должна вычислять скидку, затем прибавлять налог и возвращать итоговую цену.
+
+// Пример работы:
+// console.log(calculateFinalPrice(100, 10, 0.2)); // 108
+// console.log(calculateFinalPrice(100, 10, 0)); // 90
+
+function calculateFinalPrice(basePrice, discount, taxRate){
+    return (basePrice - (basePrice / 100 * discount)) * (1 + taxRate)
+}
+console.log(calculateFinalPrice(100, 10, 0.2))
+/* const calculateFinalPrice_Arrow = (basePrice, discount, taxRate) => (basePrice - (basePrice / 100 * discount)) * (1 + taxRate); */
+
+// Задача 2.
+// Напишите функцию checkAccess, которая принимает имя пользователя и пароль. Если имя пользователя равно "admin" и пароль равен "123456", функция должна возвращать строку "Доступ разрешен", иначе — "Доступ запрещен".
+function checkAccess(user, pass) {
+    return (user == "admin" && pass == "123456") ? "Доступ разрешен ✅" : "Доступ запрещен ❌";
+}
+
+console.log(checkAccess('admin', 123456))
+// Задача 3.
+// Напишите функцию getTimeOfDay, которая принимает текущее время (число от 0 до 23) и возвращает строку:
+// "Ночь" (с 0 до 5 часов),
+// "Утро" (с 6 до 11 часов),
+// "День" (с 12 до 17 часов),
+// "Вечер" (с 18 до 23 часов).
+// Если введённое значение не попадает в этот диапазон, возвращайте `"Некорректное время"`.
+function getTimeOfDay(time) {
+    return time > 0 && time <= 5 ? "Ночь" : time <= 11 ? "Утро" : time <= 17 ? "День" : time <= 23 ? "Вечер" : "Некорректное время"; 
+}
+const getTimeOfDay_Arrow = time => time > 0 && time <= 5 ? "Ночь" : time <= 11 ? "Утро" : time <= 17 ? "День" : time <= 23 ? "Вечер" : "Некорректное время";
+console.log(getTimeOfDay_Arrow(16))
+console.log(getTimeOfDay(16))
+// Задача 4.
+// Напишите функцию findFirstEven, которая принимает два числа start и end и находит первое чётное число в указанном диапазоне.
+// Если чётного числа в этом диапазоне нет, функция должна вернуть "Чётных чисел нет".
+function findFirstEven(start, end){
+  return start % 2 == 0 ? start : start != end ? start + 1 : "Чётных чисел нет";
+}
+console.log(findFirstEven(9,10))
+// Пример работы:
+// console.log(findFirstEven(1, 10)); // 2
+// console.log(findFirstEven(9, 9)); // "Чётных чисел нет"
+
