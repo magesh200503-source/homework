7bd16e76c292452f248d42866c8f63f027ba0c10
magesh <magesh200503@gmail.com>
2026-09-03 23:22:25 +0300

    Add files via upload

---
+++ main-89/js/main.js
@@ -0,0 +1,12 @@
+import { getTodosFromLocalStorage} from "./storage.js"
+import { renderTodos, initTodoHandlers, } from "./dom.js"
+
+
+
+const todos = getTodosFromLocalStorage() || [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderTodos(todos)
+    initTodoHandlers(todos)
+})
+
